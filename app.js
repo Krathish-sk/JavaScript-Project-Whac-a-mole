37ae970db554565e08115702cfe55998cd6e9f63
Krathish sk <krathishs.k123@gmail.com>
2023-01-16 10:29:39 +0530

    adding a mole randomly

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
+
+
+//Removing the mole, which means creatina new  grid without a mole
+
+function randomSquare(){
+    squares.forEach(square =>{
+        square.callList.remove('mole')
+    })
+
+
+    let randomSquare = square[Math.floor(Math.random() * 9)]
+    randomeSquare.classList.add('.mole')
+
+
+}
