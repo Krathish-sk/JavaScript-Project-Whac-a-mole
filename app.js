2c1697f9a2761b5da417850378d7cbf386f30b7f
Krathish sk <krathishs.k123@gmail.com>
2023-01-16 10:57:10 +0530

    increase the score on click

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 
+let result=0
+let hitPosition
+
 //Removing the mole, which means creatina new  grid without a mole
 function randomSquare(){
     squares.forEach(square =>{
@@ -13,7 +16,21 @@ function randomSquare(){
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole');
+
+    hitPosition = randomSquare.id
 }
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent=result
+            hitPosition=null
+        }
+    })
+})
+
+
 
 function moveMole(){
     let timerId= null
